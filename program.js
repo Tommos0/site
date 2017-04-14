4cde0dfedc11506fa9e305a7199f05ce90739187
Tom Klaver <tomklav@gmail.com>
2017-04-15 01:39:39 +0200

    suus is lief

--- program.js
+++ program.js
@@ -18,15 +18,6 @@ var script = document.createElement('script'); script.src = 'https://code.jquery
 $("#content").css('color' , 'red');
 //Seems fine! I have to do something about the display, maybe your screen is getting full of text..
 //I can modify the function the displays my texts a little:
-//It is currently this:
-//processLine = function (lineNumber) {
-//    var par = document.createElement('p');
-//    var t = document.createTextNode(lines[lineNumber]);
-//    par.appendChild(t);
-//    document.getElementById('content').appendChild(par);
-//    eval(lines[lineNumber]);
-//    if (lineNumber + 1 < lines.length) window.setTimeout(function () { processLine(lineNumber + 1)}, 500); };
-// });
 processLine = function (lineNumber) { var duration = lines[lineNumber].split("|")[0];var code = lines[lineNumber].slice(duration.length+1);$("#content").html(code); eval(code); if (lineNumber + 1 < lines.length) window.setTimeout(function () {processLine(lineNumber + 1)}, parseInt(duration));};
 2000|//i hope this is still working :)
 2000|//Took me a bit to get this to work but I'm back in business and I have a new thing to play with,
@@ -38,38 +29,38 @@ processLine = function (lineNumber) { var duration = lines[lineNumber].split("|"
 3000|//Or...
 3000|//Really...
 3000|//Slow...
-1|//1000|$('body').css({'background-color':'#555', 'width':'100%', 'height':'100%'}); //dark background
-//If you're interested: this is the html that is loaded and that loads this file:
+1000|//If you're interested: this is the html that is loaded and that loads this file:
 5000|var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function () {if (this.readyState == 4 && this.status == 200) {$("#content").css('white-space','pre').text(this.response);}};xhttp.open("GET", "index.html", true); xhttp.send();
 5000|//and the rest is this file (I wonder if I can fetch my own code?)
 15000|var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function () {if (this.readyState == 4 && this.status == 200) {$("#content").css('white-space','pre').text(this.response);}};xhttp.open("GET", "program.js", true); xhttp.send();
 3000|//OK, it's about time to make a real website..
-1000|//Hello there!
-1000|//I'm back - this is my second evening already working on this thing.
-1000|//Feeling a bit strange about typing messages to my computer :).
-1000|//I was thinking about a couple of things for this website...
-1000|//
-1000|//First
-1000|//Why am I even writing this thing?
-1000|//It's strange now typing to a screen.
-1000|//I think my idea is to publicize it somewhere.
-1000|//So I will rent some server space to host it.
-1000|//
-1000|//Second
-1000|//I wonder if somebody will ever read this?
-1000|//But if you are -
-1000|//So for I've only been talking to you
-1000|//I will make an input field in which you can type something back to me!
-1000|//
-1000|//Third
-1000|//This website needs a name
-1000|//I decided on something simple -
-1000|//The website will be named after its assigned IP address
-1000|//This way I also don't need a domain name :).
-1000|//
-1000|//Fourth
-1000|//I want to do something fancy for these messages
-1000|//
-1000|//Ok I've got the server! This means that the name of the website is from now on 35.160.143.3
-document.title = '35.160.143.3';
-
+3000|//Hello there!
+3000|//I'm back - this is my second evening already working on this thing.
+3000|//Feeling a bit strange about typing messages to my computer :).
+3000|//I was thinking about a couple of things for this website...
+3000|//
+3000|//First
+3000|//Why am I even writing this thing?
+3000|//It's strange now typing to a screen.
+3000|//I think my idea is to publicize it somewhere.
+3000|//So I will rent some server space to host it.
+3000|//
+3000|//Second
+3000|//I wonder if somebody will ever read this?
+3000|//But if you are -
+3000|//So for I've only been talking to you
+3000|//I will make an input field in which you can type something back to me!
+3000|//
+3000|//Third
+3000|//This website needs a name
+3000|//I decided on something simple -
+3000|//The website will be named after its assigned IP address
+3000|//This way I also don't need a domain name :).
+3000|//
+3000|//Fourth
+3000|//I want to do something fancy for these messages
+3000|//
+3000|//Ok I've got the server! This means that the name of the website is from now on 35.160.143.3
+3000|document.title = '35.160.143.3';
+3000|//We're live! I'm still changing the timings a little bit..
+10000|//Suus is lief
