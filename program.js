c4ab23f5dc2f3beb5683f3b089c2fcfca75540b8
Tom Klaver <tomklav@gmail.com>
2017-04-15 04:20:10 +0200

    fix

--- program.js
+++ program.js
@@ -78,6 +78,6 @@ processLine = function (lineNumber) { var duration = lines[lineNumber].split("|"
 3000|//and implement sendMessage
 3000|sendMessage = function(message) { $.post('/server/message', message , function() { messageDone() }, 'text'); };
 3000|thankYou = '<span>Thank you!</span>';
-3000|messageDone = function() { var elmThankYou = $(thankYou); elmThankYou.insertAfter(button); setTimeout(function() { elmThankYou.remove, 500 }); };
+3000|messageDone = function() { var elmThankYou = $(thankYou); elmThankYou.insertAfter(button); setTimeout(function() { elmThankYou.remove; }, 500); };
 10000|showCode('server.py');
 10000|showCode('nginx.conf');
